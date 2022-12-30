23c7021cbd26797d52dd877dab7a39ea479f3965
Nakorn Sinpadung <nakorndev@gmail.com>
2022-12-30 10:55:51 +0700

    เพิ่มการเข้าสู่ระบบ และระบบ Session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const app = express();
 
@@ -16,6 +17,15 @@ const knex = require('knex')({
 app.use(express.static('public'));
 app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 3 // 3 days
+  }
+}));
 
 app.set('layout', 'layout.ejs');
 app.set('view engine', 'ejs');
@@ -26,6 +36,36 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
   res.render('login', { title: 'Login' });
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  // Check if the user exists in the database
+  knex('users').where('email', email).first().then(user => {
+    if (user) {
+      // Compare the passwords
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (isMatch) {
+          // Password is correct
+          req.session.user = user;
+          req.session.isLoggedIn = true;
+          req.session.save(() => {
+            res.redirect('/');
+          });
+        } else {
+          // Password is incorrect
+          res.send('Incorrect password');
+        }
+      });
+    } else {
+      // User does not exist
+      res.send('User does not exist');
+    }
+  }).catch(err => {
+    console.error(err);
+    res.send('An error occurred');
+  });
 });
 
 app.get('/sign-up', (req, res) => {
