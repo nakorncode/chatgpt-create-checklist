90dd31578a0590f7bb7dd5675f3aa7d322aed1c3
Nakorn Sinpadung <nakorndev@gmail.com>
2022-12-30 10:01:15 +0700

    แก้ไม่ได้ติดตั้ง ejs layout และโค้ดไม่ถูกต้อง

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
 app.use(express.static('public'));
+app.use(expressLayouts);
 
+app.set('layout', 'layout.ejs');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
